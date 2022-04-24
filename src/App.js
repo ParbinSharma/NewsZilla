72470fb9d912f351b8fad45a8585418cba3aeecf
ParbinDev <sharmaparbin91@gmail.com>
2022-04-24 14:23:04 +0530

    Commit message

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+import React, { useState } from 'react'
+import NavBar from './navbar.js';
+import News from './news.js';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+const App = ()=> {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  
+ 
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+
+        <Routes>
+          <Route exact path="/" element={<News   apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+
+          <Route exact path="/business" element={<News   apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/>}/>
+          <Route exact path="/entertainment" element={<News   apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/>
+
+          <Route exact path="/health" element={<News   apiKey={apiKey} key="/health" pageSize={pageSize} country="in" category="health"/>}/>
+          <Route exact path="/science" element={<News   apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/>}/>
+          <Route exact path="/sports" element={<News   apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/>}/>
+          <Route exact path="/technology" element={<News   apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}/>
+
+        </Routes>
+        </Router>
+      </div>
+    )
+ 
+}
+
+export default App;
